5fbc482bb2ea61bfdb1452da0342839707fa7276
Tatiana Lyapina <antipts@gmail.com>
2022-06-30 00:43:20 +0300

    fixes #5 Fixed method getSumPlayedTime

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -73,6 +73,10 @@ public class GameStore {
      * за играми этого каталога
      */
     public int getSumPlayedTime() {
-        return 0;
+        int sumTime = 0;
+        for (int hour : playedTime.values()) {
+            sumTime += hour;
+            }
+        return sumTime;
     }
 }
